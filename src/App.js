ddfa2a9e29785c5796aa6f322b9b02d4382b9cd0
bltt84 <blttt909@gmail.com>
2021-02-09 20:00:05 +0700

    Updating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
@@ -15,7 +11,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path='/' component={ HomePage } />
-          <Route exact path='/hats' component={ HatsPage } />
+          <Route exact path='/shop' component={ ShopPage } />
         </Switch>
       </div>
     </BrowserRouter>
